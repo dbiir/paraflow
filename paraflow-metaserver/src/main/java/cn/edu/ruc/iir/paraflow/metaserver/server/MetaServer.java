9dccfcea69f03f78bae2fc1cd627057779cfb8a6
Guodong Jin <jelly.guodong.jin@gmail.com>
2017-08-01 18:18:41 +0800

    basic meta server main loop

--- paraflow-metaserver/src/main/java/cn/edu/ruc/iir/paraflow/metaserver/server/MetaServer.java
+++ paraflow-metaserver/src/main/java/cn/edu/ruc/iir/paraflow/metaserver/server/MetaServer.java
@@ -1,11 +1,15 @@
 package cn.edu.ruc.iir.paraflow.metaserver.server;
 
+import cn.edu.ruc.iir.paraflow.commons.exceptions.RPCServerIOException;
 import cn.edu.ruc.iir.paraflow.metaserver.service.MetaService;
+import cn.edu.ruc.iir.paraflow.metaserver.utils.DBConnection;
+import cn.edu.ruc.iir.paraflow.metaserver.utils.MetaConfig;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 /**
  * ParaFlow
@@ -14,47 +18,97 @@ import java.util.logging.Logger;
  */
 public class MetaServer
 {
-    private static final Logger logger = Logger.getLogger(MetaServer.class.getName());
+    private static final Logger logger = LogManager.getLogger(MetaServer.class);
 
     private Server server;
+    private final String metaConfigPath;
 
-    public void start(int port) throws IOException
+    public MetaServer(String metaConfigPath)
     {
-        server = ServerBuilder.forPort(port)
-                .addService(new MetaService())
-                .build()
-                .start();
-        logger.info("Server started at port " + port);
+        this.metaConfigPath = metaConfigPath;
+    }
 
+    /**
+     * Start meta server.
+     * 1. Start rpc server
+     * 2. Connect to database
+     *
+     * @param port rpc server port
+     * */
+    public void start(int port) throws RPCServerIOException
+    {
+        // start gRPC server
+        try {
+            server = ServerBuilder.forPort(port)
+                    .addService(new MetaService())
+                    .build()
+                    .start();
+        }
+        catch (IOException e) {
+            throw new RPCServerIOException(port);
+        }
         Runtime.getRuntime().addShutdownHook(
                 new Thread(MetaServer.this::stop)
         );
+        logger.info("****** RPC server started at port " + port);
+        System.out.println("****** RPC server started at port " + port);
+
+        // get config instance
+        MetaConfig metaConfig = new MetaConfig(metaConfigPath);
+
+        // connect database
+        DBConnection.connect(
+                metaConfig.getDBDriver(),
+                metaConfig.getDBHost(),
+                metaConfig.getDBUser(),
+                metaConfig.getDBPassword());
+        logger.info("****** Database connected successfully");
+        System.out.println("****** Database connected successfully");
+
+        logger.info("====== MetaServer started successfully ======");
+        System.out.println("====== MetaServer started successfully ======");
     }
 
+    /**
+     * Stop meta server and stop all sub-modules
+     * 1. Stop rpcServer
+     * 2. Stop db connection
+     * */
     public void stop()
     {
         if (server != null) {
-            logger.info("***Server shutting down...");
+            logger.info("****** MetaServer shutting down...");
+            System.out.println("****** MetaServer shutting down...");
             server.shutdown();
-            logger.info("***Server has shut down.");
+            DBConnection.getConnectionInstance().close();
+            logger.info("====== MetaServer has shut down. ======");
+            System.out.println("====== MetaServer shutting down ======");
         }
     }
 
-    public void blockUntilTermination() throws InterruptedException
+    /**
+     * Await for interrupted signal to stop server
+     * */
+    public void blockUntilTermination()
     {
         if (server != null) {
-            server.awaitTermination();
+            try {
+                server.awaitTermination();
+            }
+            catch (InterruptedException e) {
+                stop();
+            }
         }
     }
 
     public static void main(String[] args)
     {
-        MetaServer server = new MetaServer();
+        MetaServer server = new MetaServer(args[1]);
         try {
             server.start(10012);
             server.blockUntilTermination();
         }
-        catch (IOException | InterruptedException e) {
+        catch (RPCServerIOException e) {
             e.printStackTrace();
         }
     }
--- paraflow-metaserver/src/main/java/cn/edu/ruc/iir/paraflow/metaserver/utils/MetaConfig.java
+++ paraflow-metaserver/src/main/java/cn/edu/ruc/iir/paraflow/metaserver/utils/MetaConfig.java
@@ -1,5 +1,7 @@
 package cn.edu.ruc.iir.paraflow.metaserver.utils;
 
+import cn.edu.ruc.iir.paraflow.commons.utils.ConfigFactory;
+
 /**
  * ParaFlow
  *
@@ -7,4 +9,30 @@ package cn.edu.ruc.iir.paraflow.metaserver.utils;
  */
 public class MetaConfig
 {
+    private ConfigFactory configInstance = null;
+
+    public MetaConfig(String configPath)
+    {
+        configInstance = ConfigFactory.getConfigInstance(configPath);
+    }
+
+    public String getDBDriver()
+    {
+        return configInstance.getProperty("db.driver");
+    }
+
+    public String getDBHost()
+    {
+        return configInstance.getProperty("db.host");
+    }
+
+    public String getDBUser()
+    {
+        return configInstance.getProperty("db.user");
+    }
+
+    public String getDBPassword()
+    {
+        return configInstance.getProperty("db.password");
+    }
 }
