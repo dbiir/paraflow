da0b363e829df2624e9251678ab9faa76c82b482
Guodong Jin <jelly.guodong.jin@gmail.com>
2017-12-13 18:49:15 +0800

    connector

--- paraflow-connector/src/main/java/cn/edu/ruc/iir/paraflow/connector/ParaflowConnector.java
+++ paraflow-connector/src/main/java/cn/edu/ruc/iir/paraflow/connector/ParaflowConnector.java
@@ -5,6 +5,13 @@ import com.facebook.presto.spi.connector.ConnectorMetadata;
 import com.facebook.presto.spi.connector.ConnectorSplitManager;
 import com.facebook.presto.spi.connector.ConnectorTransactionHandle;
 import com.facebook.presto.spi.transaction.IsolationLevel;
+import com.google.inject.Inject;
+import sun.plugin.viewer.LifeCycleManager;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * paraflow
@@ -14,6 +21,27 @@ import com.facebook.presto.spi.transaction.IsolationLevel;
 public class ParaflowConnector
         implements Connector
 {
+    private final LifeCycleManager lifeCycleManager;
+    private final ParaflowMetadataFactory paraflowMetadataFactory;
+    private final ParaflowSplitManager paraflowSplitManager;
+    private final ParaflowPageSourceProvider paraflowPageSourceProvider;
+
+    private final ConcurrentMap<ConnectorTransactionHandle, ParaflowMetadata> transactions =
+            new ConcurrentHashMap<>();
+
+    @Inject
+    public ParaflowConnector(
+                    LifeCycleManager lifeCycleManager,
+                    ParaflowMetadataFactory paraflowMetadataFactory,
+                    ParaflowSplitManager paraflowSplitManager,
+                    ParaflowPageSourceProvider paraflowPageSourceProvider)
+    {
+        this.lifeCycleManager = requireNonNull(lifeCycleManager, "lifeCycleManager lis null");
+        this.paraflowMetadataFactory = requireNonNull(paraflowMetadataFactory, "paraflowMetadataFactory is null");
+        this.paraflowSplitManager = requireNonNull(paraflowSplitManager, "paraflowSplitManager is null");
+        this.paraflowPageSourceProvider = requireNonNull(paraflowPageSourceProvider, "paraflowPageSourceProvider is null");
+    }
+
     @Override
     public ConnectorTransactionHandle beginTransaction(IsolationLevel isolationLevel, boolean readOnly)
     {
